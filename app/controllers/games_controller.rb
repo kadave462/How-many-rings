79bf6601d48e19073005757af1b199bbcd91d63b
julien galeron <julien.galeron@icloud.com>
2024-03-19 17:48:39 +0100

    Little update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
     @offers = Offer.all
+  end
 
   before_action :set_game, only: [:show]
   # before_action :set_game, only: [:show, :edit, :update, :destroy](change this line later)
