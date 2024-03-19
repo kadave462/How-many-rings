3deb57041a44967e2a5432063e2228269e657095
julien galeron <julien.galeron@icloud.com>
2024-03-19 11:18:14 +0100

    Routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :games do
+    resources :owned_games
+  end
+
+  resources :owned_games
+
+  get 'account', to: "pages#account"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
