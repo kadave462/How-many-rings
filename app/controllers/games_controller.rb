c268eb49ef2f9a5d206388279157e5784c305479
julien galeron <julien.galeron@icloud.com>
2024-03-22 16:09:07 +0100

    Job done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,11 @@
 class GamesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_game, only: [:show]
 
   def index
     @query = params[:query]
     if @query.blank?
-      @games = Game.all
+      @games = Game.all.order(created_at: :desc)
     else
       @games = Game.where('title ILIKE ?', "%#{@query}%")
     end
