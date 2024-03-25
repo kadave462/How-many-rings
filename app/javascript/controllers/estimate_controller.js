37b4177da40e5e6056378b3b829caca911f4af5b
julien galeron <julien.galeron@icloud.com>
2024-03-25 16:28:57 +0100

    Mid-day WIP

--- app/javascript/controllers/estimate_controller.js
+++ app/javascript/controllers/estimate_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="estimate"
 export default class extends Controller {
-  static targets = ["addButton", "box", "cover", "manual", "game", "price", "estimatedPrice"]
+  static targets = ["addButton", "box", "cover", "manual", "game", "userPrice", "estimatedPrice", "saleInfos", "firstAddButton"]
   static values = {
     price: Number,
   }
@@ -14,6 +14,18 @@ export default class extends Controller {
   displaySubmitArea() {
     this.estimatedPriceTarget.innerText = this.estimate().toFixed(2)
     this.addButtonTarget.classList.remove("d-none")
+  }
+
+  onSale() {
+    this.saleInfosTarget.classList.remove("d-none")
+    this.firstAddButtonTarget.classList.add("d-none")
+    this.userPriceTarget.setAttribute("required", true)
+  }
+
+  notOnSale() {
+    this.saleInfosTarget.classList.add("d-none")
+    this.firstAddButtonTarget.classList.remove("d-none")
+    this.userPriceTarget.removeAttribute("required")
   }
 
   estimate() {
